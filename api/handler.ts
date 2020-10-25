fe549b61b0ed7966c76d475652a5f490c81b5d80
karosi12 <adekayor@gmail.com>
2020-10-25 12:25:48 +0100

    check server health

--- api/handler.ts
+++ api/handler.ts
@@ -1,12 +1,18 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayEvent, Context, Callback } from 'aws-lambda';
 import 'source-map-support/register';
-
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+export const ping: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+  _context: Context,
+  callback: Callback,
+): Promise<any> => {
+  try {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `server is running`,
+      }),
+    });
+  } catch (error) {
+    callback(null, { statusCode: 500, body: JSON.stringify({ message: 'Something went wrong' }) });
+  }
+};
