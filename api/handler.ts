a249d636ba56a7286536bc9d3bf7456f82e3e0db
karosi12 <adekayor@gmail.com>
2020-10-25 14:56:16 +0100

    added user message to queue

--- api/handler.ts
+++ api/handler.ts
@@ -1,4 +1,6 @@
 import { APIGatewayProxyHandler, APIGatewayEvent, Context, Callback } from 'aws-lambda';
+import IMessageBody from './interfaces';
+import { sendDataToSQS } from './services/queueServices';
 import 'source-map-support/register';
 export const ping: APIGatewayProxyHandler = async (
   event: APIGatewayEvent,
@@ -6,13 +8,28 @@ export const ping: APIGatewayProxyHandler = async (
   callback: Callback,
 ): Promise<any> => {
   try {
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `server is running`,
-      }),
-    });
+    callback(null, { statusCode: 200, body: JSON.stringify({ message: `server is running` }) });
   } catch (error) {
     callback(null, { statusCode: 500, body: JSON.stringify({ message: 'Something went wrong' }) });
+  }
+};
+
+export const addMsgToQueue: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+  _context: Context,
+  callback: Callback,
+): Promise<any> => {
+  const requestBody: IMessageBody = JSON.parse(event.body);
+  try {
+    if (!requestBody.message)
+      callback(null, { statusCode: 400, body: JSON.stringify({ message: 'message is required' }) });
+    if (requestBody.email || requestBody.phoneNumber) {
+      const { data, message } = await sendDataToSQS(requestBody);
+      callback(null, { statusCode: 200, body: JSON.stringify({ message, data }) });
+    } else {
+      callback(null, { statusCode: 400, body: JSON.stringify({ message: 'email or phone number is required' }) });
+    }
+  } catch (error) {
+    callback(null, { statusCode: 500, body: JSON.stringify({ message: 'Internal server error' }) });
   }
 };
